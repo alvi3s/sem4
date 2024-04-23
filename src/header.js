e84ed909956a8deee9a8aa6b9e5d3925a0f6c778
alvi3s <166526027+alvi3s@users.noreply.github.com>
2024-04-23 15:38:08 +0000

    final

--- src/header.js
+++ src/header.js
@@ -1,23 +1,25 @@
 const Header = () => {
   return (
     <div style={{ textAlign: 'center' }}>
-      <h1 style={{ margin: '0 auto', fontSize: '4rem', color: '#003366' }}>YAMRUN¡</h1>
+      <h1 style={{ margin: '0 auto', fontSize: '4rem', color: '#003366' }}>
+        YAMRUN¡ <i className="bi bi-airplane-engines"></i>
+      </h1>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">INICIO</a>
+        <i data-feather="home"></i><a className="navbar-brand" href="#">INICIO</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">PRECIOS</a>
+              <a className="nav-link active" aria-current="page" href="#"><i data-feather="dollar-sign"></i> PRECIOS</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">CONSULTAS FRECUENTES</a>
+                <a className="nav-link" href="#"><i data-feather="help-circle"></i>CONSULTAS FRECUENTES</a>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i data-feather="send"></i>
                   MODELOS
                 </a>
                 <ul className="dropdown-menu">
--- src/main.js
+++ src/main.js
@@ -1,46 +1,43 @@
+import Carousel from 'react-bootstrap/Carousel';
+
 const Main = () => {
-    return (
-      <div id="carouselExampleCaptions" className="carousel slide">
-        <div className="carousel-indicators">
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-          <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
-        <div className="carousel-inner">
-          <div className="carousel-item active">
-            <img src="https://d1l2ivnr70xlj8.cloudfront.net/sami3-prod/kawasaki.com.pe/images/productos/nonj400sliviana-K2EX400KRTNNGY2T0297.png" className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h5 style={{ color: 'black' }}>NINJA 400 ABS KRT EDITION</h5>
-              <p></p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            <img src="https://fulltienda.cl/wp-content/uploads/2022/01/bajaj-pulsar-rs200-nuevo-diseno-2021-rojo-e4b698.png" className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h5 style={{ color: 'black' }}>PULSAR RS 200</h5>
-              <p></p>
-            </div>
-          </div>
-          <div className="carousel-item">
-            {/* Comentario eliminado porque no es válido en JSX */}
-            <img src="https://motorlandperu.com/wp-content/uploads/2023/09/PHO_BIKE_90_REVO_MY24-KTM-250-DUKE-front-rightt_SALL_AEPI_V1.png" className="d-block w-100" alt="..."/>
-            <div className="carousel-caption d-none d-md-block">
-              <h5 style={{ color: 'black' }}>DUKE 250 ABS 2024 KTM</h5>
-              <p></p>
-            </div>
-          </div>
-        </div>
-        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Previous</span>
-        </button>
-        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-          <span className="carousel-control-next-icon" aria-hidden="true"></span>
-          <span className="visually-hidden">Next</span>
-        </button>
-      </div>
-    );
-  }
-  
-  export default Main;
-  
+  return (
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img
+          className="d-block w-100"
+          src="https://d1l2ivnr70xlj8.cloudfront.net/sami3-prod/kawasaki.com.pe/images/productos/nonj400sliviana-K2EX400KRTNNGY2T0297.png"
+          alt="NINJA 400 ABS KRT EDITION"
+        />
+        <Carousel.Caption>
+          <h3>NINJA 400 ABS KRT EDITION</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+        <img
+          className="d-block w-100"
+          src="https://fulltienda.cl/wp-content/uploads/2022/01/bajaj-pulsar-rs200-nuevo-diseno-2021-rojo-e4b698.png"
+          alt="PULSAR RS 200"
+        />
+        <Carousel.Caption>
+          <h3>PULSAR RS 200</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="https://motorlandperu.com/wp-content/uploads/2023/09/PHO_BIKE_90_REVO_MY24-KTM-250-DUKE-front-rightt_SALL_AEPI_V1.png"
+          alt="DUKE 250 ABS 2024 KTM"
+        />
+        <Carousel.Caption>
+          <h3>DUKE 250 ABS 2024 KTM</h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Main;
